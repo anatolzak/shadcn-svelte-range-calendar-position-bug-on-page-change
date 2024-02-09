cbfd37b4f44514f23c12b8ae19e4ccd0f510b57f
anatolzak <53095479+anatolzak@users.noreply.github.com>
2024-02-09 15:30:46 +0200

    added popover position bug reproduction

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,21 @@
 <div class="flex space-x-3 p-4">
   <a
     class="border-b border-b-muted-foreground"
-    href="/">Range Calendar</a
+    href="/"
   >
+    Range Calendar
+  </a>
   <a
     class="border-b border-b-muted-foreground"
-    href="/blank">Blank Page</a
+    href="/blank"
   >
+    Blank Page
+  </a>
+  <a
+    class="border-b border-b-muted-foreground"
+    href="/popover"
+  >
+    Popover
+  </a>
 </div>
 <slot />
---
+++ src/routes/popover/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button';
+  import * as Popover from '$lib/components/ui/popover';
+</script>
+
+<Popover.Root portal={null}>
+  <Popover.Trigger
+    asChild
+    let:builder
+  >
+    <Button
+      builders={[builder]}
+      variant="outline">Open</Button
+    >
+  </Popover.Trigger>
+  <Popover.Content class="w-80">
+    <div class="grid gap-4">
+      <div class="space-y-2">
+        <h4 class="font-medium leading-none">Dimensions</h4>
+        <p class="text-sm text-muted-foreground">Set the dimensions for the layer.</p>
+      </div>
+    </div>
+  </Popover.Content>
+</Popover.Root>
