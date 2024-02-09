9fc0bc2ed8e9f73d536b9ed330acbef9ea6b5b0e
anatolzak <53095479+anatolzak@users.noreply.github.com>
2024-02-09 15:27:11 +0200

    added range calendar position bug reproduction

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,14 @@
   import '../app.pcss';
 </script>
 
+<div class="flex space-x-3 p-4">
+  <a
+    class="border-b border-b-muted-foreground"
+    href="/">Range Calendar</a
+  >
+  <a
+    class="border-b border-b-muted-foreground"
+    href="/blank">Blank Page</a
+  >
+</div>
 <slot />
