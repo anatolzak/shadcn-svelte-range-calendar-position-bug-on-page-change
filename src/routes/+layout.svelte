3bb1ba0252c03ca911c33bfc336dd4f66fcc44d2
anatolzak <53095479+anatolzak@users.noreply.github.com>
2024-02-22 15:42:11 +0200

    refactor layout links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,38 @@
-<script>
+<script lang="ts">
   import '../app.pcss';
+  import { page } from '$app/stores';
+
+  const links: { href: string; label: string }[] = [
+    {
+      href: '/blank',
+      label: 'Blank Page',
+    },
+    {
+      href: '/',
+      label: 'Bits-UI Popover',
+    },
+    {
+      href: '/melt-ui-popover',
+      label: 'Melt-UI Popover',
+    },
+    {
+      href: '/slow',
+      label: 'Slow Navigation',
+    },
+  ];
 </script>
 
-<div class="flex space-x-3 p-4">
-  <a
-    class="border-b border-b-muted-foreground"
-    href="/blank"
-  >
-    Blank Page
-  </a>
-  <a
-    class="border-b border-b-muted-foreground"
-    href="/"
-  >
-    Popover
-  </a>
-  <a
-    class="border-b border-b-muted-foreground"
-    href="/slow"
-  >
-    Slow Navigation
-  </a>
-  <a
-    class="border-b border-b-muted-foreground"
-    href="/melt-ui-popover"
-  >
-    Melt-UI Popover
-  </a>
+<div class="p-10">
+  <div class="flex space-x-3 py-4">
+    {#each links as { href, label }}
+      {@const isActive = $page.url.pathname === href}
+      <a
+        class={` ${isActive ? 'border-b border-b-muted-foreground' : 'text-gray-400'}`}
+        {href}
+      >
+        {label}
+      </a>
+    {/each}
+  </div>
+  <slot />
 </div>
-<slot />
