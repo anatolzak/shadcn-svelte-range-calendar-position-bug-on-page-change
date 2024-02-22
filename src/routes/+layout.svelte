4f5ab106f71c5cf5152c3823f5509bc0d99c8cbb
anatolzak <53095479+anatolzak@users.noreply.github.com>
2024-02-22 15:38:21 +0200

    removed range calendar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,6 @@
 <div class="flex space-x-3 p-4">
   <a
     class="border-b border-b-muted-foreground"
-    href="/"
-  >
-    Range Calendar
-  </a>
-  <a
-    class="border-b border-b-muted-foreground"
     href="/blank"
   >
     Blank Page
